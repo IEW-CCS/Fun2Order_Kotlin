0802919b88ff49e4858595f6e4812853d9268826
“Chris” <IEW@gmail.com>
2020-06-12 11:58:07 +0800

    1. Splash Bug Fix and 固定不要旋轉

--- app/src/main/java/com/iew/fun2order/Splash.kt
+++ app/src/main/java/com/iew/fun2order/Splash.kt
@@ -16,21 +16,16 @@ class Splash : AppCompatActivity() {
         setContentView(R.layout.launch_screen)
         val secondsDelayed = 1
         Handler().postDelayed(Runnable {
-            val mAuth = FirebaseAuth.getInstance()
-            if (mAuth.currentUser != null) {
-                val newIntent = Intent()
-                newIntent.setClass(this@Splash, Logon::class.java)
-                newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                if (intent.extras != null) {
-                    newIntent.putExtras(intent!!.extras!!)   // 如果有帶入Notification Info 就帶進去Main畫面處理
-                }
-                else
-                {
-                    newIntent.putExtras(Bundle())            //沒有就帶空的值進去
-                }
-                startActivity(newIntent)
-                this.finish()
+            val newIntent = Intent()
+            newIntent.setClass(this@Splash, Logon::class.java)
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            if (intent.extras != null) {
+                newIntent.putExtras(intent!!.extras!!)   // 如果有帶入Notification Info 就帶進去Main畫面處理
+            } else {
+                newIntent.putExtras(Bundle())            //沒有就帶空的值進去
             }
-        }, secondsDelayed*1000L)
+            startActivity(newIntent)
+            this.finish()
+        }, secondsDelayed * 1000L)
     }
 }
